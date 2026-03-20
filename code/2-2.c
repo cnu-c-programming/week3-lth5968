43d7a356d115af687c7de08db1441ca4c61cac6f
lth5968 <lth5968@gmail.com>
2026-03-20 09:40:17 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int x*, int y*) {
+    
+}
+
+int main() {
+    int x = 100;
+    int y = 200;
+
+    printf("x = %d, y = %d", x ,y);
+    swap();
+    printf("x = %d, y = %d", x ,y);
+    
+    return 0;
+}
