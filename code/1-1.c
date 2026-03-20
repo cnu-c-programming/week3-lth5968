2979876e85b1a3111105640576d034ddcf906b74
lth5968 <lth5968@gmail.com>
2026-03-20 09:02:42 +0900

    Update 1-1.c

---
+++ code/1-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 10;
+    int b = 5;
+    printf("10 +  5 = %d \n", a+b);
+    printf("10 -  5 = %d \n", a-b);
+    printf("10 *  5 = %d \n", a*b);
+    printf("10 /  5 = %d \n", a/b);
+
+    return 0;
+}
