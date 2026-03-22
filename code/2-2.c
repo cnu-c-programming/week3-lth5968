be3eea74df460ee35c817d7ae6ec17fe3402f945
lth5968 <lth5968@gmail.com>
2026-03-22 15:34:41 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-void swap(int x*, int y*) {
+void swap(int *x, int *y) {
+    int pointer = *x;
+    *x = *y;
+    *y = pointer;
     
 }
 
@@ -8,9 +11,9 @@ int main() {
     int x = 100;
     int y = 200;
 
-    printf("x = %d, y = %d", x ,y);
-    swap();
-    printf("x = %d, y = %d", x ,y);
+    printf("x = %d, y = %d\n", x ,y);
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x ,y);
     
     return 0;
 }
