70470632b26a94f2c68a59a2b2f9432da7f70366
lth5968 <lth5968@gmail.com>
2026-03-22 14:43:09 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 
 int power(int base, int exp) {
-    if(exp<=1) {
-        return base;
-    }
-    else {
-        exp--;
-        base = base*base;
-        power(base, exp);
-    }
+    if (exp == 0)
+        return 1;
+
+    return base * power(base, exp - 1);
 }
 
 int main() {
-    printf("%d\n", power(2,3));
-    printf("%d\n", power(5,0));
-    printf("%d\n", power(3,4));
-    
+    printf("%d\n", power(2, 3));  // 8
+    printf("%d\n", power(5, 0));  // 1
+    printf("%d\n", power(3, 4));  // 81
     return 0;
 }
